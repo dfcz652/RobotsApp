20ec83dab62f36fc3947175829d784616532a52d
Andrii Sarancha <Andrii.Sarancha@nice.com>
2025-03-19 16:58:31 +0200

    Added a characteristics property and implemented a getter for it

--- RobotApp/Program.cs
+++ RobotApp/Program.cs
@@ -1,23 +1,19 @@
-﻿using System.Reflection.PortableExecutable;
-using RobotApp;
-using RobotApp.Equipment;
-using RobotApp.Equipment.Arms;
-using RobotApp.Equipment.Bodys;
-using RobotApp.Equipment.Core;
-using RobotApp.Equipment.Legs;
-using RobotApp.RobotCharacteristics;
+﻿using RobotApp.Robot.RobotEquipment.Arms;
+using RobotApp.Robot.RobotEquipment.Bodies;
+using RobotApp.Robot.RobotEquipment.Cores;
+using RobotApp.Robot.RobotEquipment.Legs;
 using RobotApp.Services;
+using RobotApp.Robot;
 
 internal class Program
 {
     private static void Main(string[] args)
     {
-        RobotService robotService = new RobotService();
+        FightingService robotService = new FightingService();
         CompareRobotCharacteristicsService compareRobotCharacteristicsService = new CompareRobotCharacteristicsService();
 
         var robot1 = new Robot();
         var robot2 = new Robot();
-        var defaultEquipRobot = new Robot();
 
         robot1.AddCore(new EnergeticCore());//Energy - 10, EnergyRestoration - 5
         robot1.AddArms(new RocketArms());//Dmg - 10, EnergyCost - 5, ImpactDistance - 6
@@ -29,12 +25,6 @@ internal class Program
         robot2.AddBody(new ShieldedBody());//Hp - 10, Shield - 10, ShieldCost - 2
         robot2.AddLegs(new ArmouredLegs());//Speed - 5, Distance - 2, Armor - 3
 
-        var allCharacteristicsOfFirstRobot = robotService.AddAllParts(robot1);
-        var allCharacteristicsOfSecondRobot = robotService.AddAllParts(robot2);
-
-        var combinedCharacteristicsOfFirstRobot = robotService.CombineCharacteristics(allCharacteristicsOfFirstRobot);
-        var combinedCharacteristicsOfSecondRobot = robotService.CombineCharacteristics(allCharacteristicsOfSecondRobot);
-
-        compareRobotCharacteristicsService.PrintCombinedCharacteristicsForTwoRobots(combinedCharacteristicsOfFirstRobot, combinedCharacteristicsOfSecondRobot);
+        compareRobotCharacteristicsService.PrintCombinedCharacteristicsForTwoRobots(robot1.RobotCharacteristics, robot2.RobotCharacteristics);
     }
 }
---
+++ RobotApp/Robot/Robot.cs
@@ -0,0 +1,59 @@
+﻿using RobotApp.Robot.Base;
+using RobotApp.Robot.RobotParts;
+
+namespace RobotApp.Robot
+{
+    internal class Robot : RobotCharacteristicsBase
+    {
+        public Core Core { get; set; }
+
+        public Body Body { get; set; }
+
+        public Arms Arms { get; set; }
+
+        public Legs Legs { get; set; }
+
+        public override List<RobotCharacteristicBase> RobotCharacteristics => CalculateRobotCharacteristics();
+
+        public Robot() : base([])
+        {
+        }
+
+        public void AddCore(Core robotPart)
+        {
+            Core = robotPart;
+        }
+
+        public void AddBody(Body robotPart)
+        {
+            Body = robotPart;
+        }
+
+        public void AddArms(Arms robotPart)
+        {
+            Arms = robotPart;
+        }
+
+        public void AddLegs(Legs robotPart)
+        {
+            Legs = robotPart;
+        }
+
+        private List<RobotCharacteristicBase> CalculateRobotCharacteristics()
+        {
+            return Core.RobotCharacteristics
+            .Concat(Arms.RobotCharacteristics)
+            .Concat(Body.RobotCharacteristics)
+            .Concat(Legs.RobotCharacteristics)
+            .GroupBy(characteristic => characteristic.GetType())
+            .Select(group =>
+            {
+                var combinedCharacteristic = (RobotCharacteristicBase)Activator.CreateInstance(group.Key);
+
+                combinedCharacteristic.Value = group.Sum(characteristic => characteristic.Value);
+
+                return combinedCharacteristic;
+            }).ToList();
+        }
+    }
+}
