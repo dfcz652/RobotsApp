5416987660bcf4c5ae2752573d9f3e311f66764e
Andrii Sarancha <Andrii.Sarancha@nice.com>
2025-04-18 12:27:33 +0300

    Fix TODO's

--- TestProject1/Tests.cs
+++ TestProject1/Tests.cs
@@ -13,7 +13,7 @@ namespace RobotAppTests
 {
     public class Tests
     {
-        private static Arms ArmsWithoutAdditionalCharacteristics = new Arms(1, 1, 1, []);
+        private static Arms ArmsWithoutAdditionalCharacteristics = new Arms(1, 1, 1, []);//??????? inline, ? ???????? ????? ????????
         private static Body BodyWithoutAdditionalCharacteristics = new Body(1, []);
         private static Core CoreWithoutAdditionalCharacteristics = new Core(1, 1, []);
         private static Legs LegsWithoutAdditionalCharacteristics = new Legs(1, 1, []);
@@ -56,89 +56,92 @@ namespace RobotAppTests
 
         public static IEnumerable<object[]> CompareCharacteristicsData =>
         new List<object[]> {
-        new object[] { new DefaultArms(),
-                    new List<RobotCharacteristicBase>() { new Dmg(5), new EnergyCost(0), new ImpactDistance(1) } },
-        new object[] { new PistolArms(),
-                    new List<RobotCharacteristicBase>() { new Dmg(7), new EnergyCost(4), new ImpactDistance(6) } },
-        new object[] { new RocketArms(),
-                    new List<RobotCharacteristicBase>() { new Dmg(10), new EnergyCost(5), new ImpactDistance(10) } },
-        new object[] { new SpearArms(),
-                    new List<RobotCharacteristicBase>() { new Dmg(12), new EnergyCost(0), new ImpactDistance(4) } },
-        new object[] { new SwordArms(),
-                    new List<RobotCharacteristicBase>() { new Dmg(15), new EnergyCost(0), new ImpactDistance(2) } },
-        new object[] { new DefaultBody(),
-            new List<RobotCharacteristicBase>() { new Hp(15) } },
-        new object[] { new ArmouredBody(),
-            new List<RobotCharacteristicBase>() { new Hp(30), new Armor(4) } },
-        new object[] { new TankyBody(),
-            new List<RobotCharacteristicBase>() { new Hp(50), new Armor(2) } },
-        new object[] { new ShieldedBody(),
-            new List<RobotCharacteristicBase>() { new Hp(10), new Shield(10), new ShieldCost(2) } },
-        new object[] { new DefaultCore(),
-            new List<RobotCharacteristicBase>() { new Energy(5), new EnergyRestoration(3) } },
-        new object[] { new EnergeticCore(),
-            new List<RobotCharacteristicBase>() { new Energy(10), new EnergyRestoration(5) } },
-        new object[] { new LivingCore(),
-            new List<RobotCharacteristicBase>() { new Energy(8), new EnergyRestoration(4), new Hp(10) } },
-        new object[] { new ProtectiveCore(),
-            new List<RobotCharacteristicBase>() { new Energy(9), new EnergyRestoration(4), new Shield(5), new ShieldCost(1) } },
-        new object[] { new DefaultLegs(),
-            new List<RobotCharacteristicBase>() { new MovementSpeed(2), new ActionSpeed(2) } },
-        new object[] { new SpeedLegs(),
-            new List<RobotCharacteristicBase>() { new MovementSpeed(10), new ActionSpeed(5) } },
-        new object[] { new ArmouredLegs(),
-            new List<RobotCharacteristicBase>() { new MovementSpeed(5), new ActionSpeed(2), new Armor(3) } },
-        new object[] { new RechargingLegs(),
-            new List<RobotCharacteristicBase>() { new MovementSpeed(5), new ActionSpeed(2), new EnergyRestoration(3) } },
+                new object[] { new DefaultArms(),
+                            new List<RobotCharacteristicBase>() { new Dmg(5), new EnergyCost(0), new ImpactDistance(1) } },
+                new object[] { new PistolArms(),
+                            new List<RobotCharacteristicBase>() { new Dmg(7), new EnergyCost(4), new ImpactDistance(6) } },
+                new object[] { new RocketArms(),
+                            new List<RobotCharacteristicBase>() { new Dmg(10), new EnergyCost(5), new ImpactDistance(10) } },
+                new object[] { new SpearArms(),
+                            new List<RobotCharacteristicBase>() { new Dmg(12), new EnergyCost(0), new ImpactDistance(4) } },
+                new object[] { new SwordArms(),
+                            new List<RobotCharacteristicBase>() { new Dmg(15), new EnergyCost(0), new ImpactDistance(2) } },
+                new object[] { new DefaultBody(),
+                    new List<RobotCharacteristicBase>() { new Hp(15) } },
+                new object[] { new ArmouredBody(),
+                    new List<RobotCharacteristicBase>() { new Hp(30), new Armor(4) } },
+                new object[] { new TankyBody(),
+                    new List<RobotCharacteristicBase>() { new Hp(50), new Armor(2) } },
+                new object[] { new ShieldedBody(),
+                    new List<RobotCharacteristicBase>() { new Hp(10), new Shield(10), new ShieldCost(2) } },
+                new object[] { new DefaultCore(),
+                    new List<RobotCharacteristicBase>() { new Energy(5), new EnergyRestoration(3) } },
+                new object[] { new EnergeticCore(),
+                    new List<RobotCharacteristicBase>() { new Energy(10), new EnergyRestoration(5) } },
+                new object[] { new LivingCore(),
+                    new List<RobotCharacteristicBase>() { new Energy(8), new EnergyRestoration(4), new Hp(10) } },
+                new object[] { new ProtectiveCore(),
+                    new List<RobotCharacteristicBase>() { new Energy(9), new EnergyRestoration(4), new Shield(5), new ShieldCost(1) } },
+                new object[] { new DefaultLegs(),
+                    new List<RobotCharacteristicBase>() { new MovementSpeed(2), new ActionSpeed(2) } },
+                new object[] { new SpeedLegs(),
+                    new List<RobotCharacteristicBase>() { new MovementSpeed(10), new ActionSpeed(5) } },
+                new object[] { new ArmouredLegs(),
+                    new List<RobotCharacteristicBase>() { new MovementSpeed(5), new ActionSpeed(2), new Armor(3) } },
+                new object[] { new RechargingLegs(),
+                    new List<RobotCharacteristicBase>() { new MovementSpeed(5), new ActionSpeed(2), new EnergyRestoration(3) } },
+        };
+
+        public static IEnumerable<object[]> AddArmsToRobotData =>
+        new List<object[]> {
+                new object[] { new Robot(), new DefaultArms(),
+                            new List<RobotCharacteristicBase>() { new Dmg(5), new EnergyCost(0), new ImpactDistance(1) } },
         };
 
         [Theory]
         [MemberData(nameof(AddCharacteristicsToPartsData))]
         public void AddCharacteristicsToPart(RobotCharacteristicsBase part, List<RobotCharacteristicBase> expectedCharacteristics)
         {
-            Assert.True(AssertEqualsCollections(expectedCharacteristics, part.RobotCharacteristics));
+            AssertEqualsCollections(expectedCharacteristics, part.RobotCharacteristics);
         }
 
         [Theory]
         [MemberData(nameof(CompareCharacteristicsData))]
-        public void CompareCharacteristics(RobotCharacteristicsBase part, List<RobotCharacteristicBase> expectedCharacteristics)
+        public void VerifyCharacteristicsOfPredefinedParts(RobotCharacteristicsBase part, List<RobotCharacteristicBase> expectedCharacteristics)
         {
-            Assert.True(AssertEqualsCollections(expectedCharacteristics, part.RobotCharacteristics));
+            AssertEqualsCollections(expectedCharacteristics, part.RobotCharacteristics);
         }
 
-        public static bool AssertEqualsCollections(List<RobotCharacteristicBase> list1, List<RobotCharacteristicBase> list2)
+        public static void AssertEqualsCollections(List<RobotCharacteristicBase> list1, List<RobotCharacteristicBase> list2)
         {
             if (list1 == null && list2 == null)
             {
-                return true;
+                Assert.Fail();
             }
             if (list1 == null || list2 == null)
             {
-                return false;
+                Assert.Fail();
             }
             if (list1.Count != list2.Count)
             {
-                return false;
+                Assert.Fail();
             }
 
-            var sortedList1 = list1.OrderBy(c => c.GetType().Name).ToList();
-            var sortedList2 = list2.OrderBy(c => c.GetType().Name).ToList();
+            list1.Sort((c1, c2) => c1.GetType().Name.CompareTo(c2.GetType().Name));
+            list2.Sort((c1, c2) => c1.GetType().Name.CompareTo(c2.GetType().Name));
 
-            for (int i = 0; i < sortedList1.Count; i++)
+            for (int i = 0; i < list1.Count; i++)
             {
-                var char1 = sortedList1[i];
-                var char2 = sortedList2[i];
-
-                if (char1.GetType() != char2.GetType())
+                if (list1[i].GetType() != list2[i].GetType())
                 {
-                    return false;
+                    Assert.Fail();
                 }
-                if(char1.Value != char2.Value)
+                if(list1[i].Value != list2[i].Value)
                 {
-                    return false;
+                    Assert.Fail();
                 }
             }
-            return true;
+            Assert.True(true);
         }
     }
 }
