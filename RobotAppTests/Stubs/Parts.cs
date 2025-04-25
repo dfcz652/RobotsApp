b606fc184e76342528fc48e620b379049f16c9ca
Andrey <sandriys2003@gmail.com>
2025-04-25 12:24:40 +0300

    Implement UnionCharacteristics and CalculateCharacteristics tests
    Implement UnionCharacteristics
    Implement CalculateCharacteristics

---
+++ RobotAppTests/Stubs/Parts.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RobotApp.Robot.Base;
+using RobotApp.Robot.RobotParts;
+
+namespace RobotAppTests.Stubs
+{
+    public class Parts
+    {
+        public class TestArms : Arms
+        {
+            public TestArms() : base(0, 0, 0, [])
+            {
+                RobotCharacteristics.Clear();
+            }
+
+            public TestArms(List<RobotCharacteristicBase> characteristics) : base(0, 0, 0, [])
+            {
+                RobotCharacteristics.Clear();
+                RobotCharacteristics = characteristics;
+            }
+        }
+
+        public class TestBody : Body
+        {
+            public TestBody() : base(0, [])
+            {
+                RobotCharacteristics.Clear();
+            }
+
+            public TestBody(List<RobotCharacteristicBase> characteristics) : base(0, [])
+            {
+                RobotCharacteristics.Clear();
+                RobotCharacteristics = characteristics;
+            }
+        }
+
+        public class TestCore : Core
+        {
+            public TestCore() : base(0, 0, [])
+            {
+                RobotCharacteristics.Clear();
+            }
+
+            public TestCore(List<RobotCharacteristicBase> characteristics) : base(0, 0, [])
+            {
+                RobotCharacteristics.Clear();
+                RobotCharacteristics = characteristics;
+            }
+        }
+
+        public class TestLegs : Legs
+        {
+            public TestLegs() : base(0, 0, [])
+            {
+                RobotCharacteristics.Clear();
+            }
+
+            public TestLegs(List<RobotCharacteristicBase> characteristics) : base(0, 0, [])
+            {
+                RobotCharacteristics.Clear();
+                RobotCharacteristics = characteristics;
+            }
+        }
+    }
+}
