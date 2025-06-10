04b577c884d09610ba1079769cfdd14695fb2db4
Andrii Sarancha <Andrii.Sarancha@nice.com>
2025-06-10 12:32:33 +0300

    Created a menu and implemented robot creating in it. Implemented CreateAndFormatReport.

--- RobotAppConsole/Program.cs
+++ RobotAppConsole/Program.cs
@@ -1,40 +1,64 @@
-﻿using RobotApp.RobotData.RobotEquipment.ArmsTypes;
-using RobotApp.RobotData.RobotEquipment.BodyTypes;
-using RobotApp.RobotData.RobotEquipment.CoreTypes;
-using RobotApp.RobotData.RobotEquipment.LegsTypes;
-using RobotApp.Services;
-using RobotApp.RobotData;
-using RobotApp.Services.Reports;
-using RobotViewModels.Interfaces;
-using RobotViewModels.Formatters;
-using RobotViewModels;
+﻿using RobotViewModels;
 
 public class Program
 {
     private static void Main(string[] args)
     {
-        IRobotsComparisonFormatter comparisonFormatter = new ReportFormatter();
-        FightingService robotService = new();
-        CompareRobotCharacteristicsService compareRobotCharacteristicsService = new();
-
-        Robot robot1 = new("BF20");
-        Robot robot2 = new("GT99");
-
-        robot1.AddCore(new EnergeticCore());
-        robot1.AddArms(new RocketArms());
-        robot1.AddBody(new ArmouredBody());
-        robot1.AddLegs(new SpeedLegs());
-
-        robot2.AddCore(new LivingCore());
-        robot2.AddArms(new SpearArms());
-        robot2.AddBody(new ShieldedBody());
-        robot2.AddLegs(new ArmouredLegs());
-
-        RobotComparisonReport report = compareRobotCharacteristicsService.CreateRobotComparisonReport(robot1, robot2);
-
         ViewModel viewModel = new();
-        viewModel.FormattedReport = comparisonFormatter.Format(report);
 
+        bool showMenu = true;
+        while (showMenu)
+        {
+            string choosedOption = DisplayMenuAndChoiceOfOption(viewModel);
+            switch (choosedOption)
+            {
+                case "1":
+                    Console.WriteLine("Hey man. Let's create a robot");
+                    Console.Write("Input robot name: ");
+                    string robotName = Console.ReadLine();
+                    Console.WriteLine("Choose arms from existing: ");
+                    foreach (string arms in viewModel.ExistingArms)
+                    {
+                        Console.WriteLine(arms);
+                    }
+                    string choosedArms = Console.ReadLine();
+
+                    Console.WriteLine("Choose body from existing: ");
+                    foreach (string body in viewModel.ExistingBodies)
+                    {
+                        Console.WriteLine(body);
+                    }
+                    string choosedBody = Console.ReadLine();
+                    Console.WriteLine("Choose core from existing: ");
+                    foreach (string core in viewModel.ExistingCores)
+                    {
+                        Console.WriteLine(core);
+                    }
+                    string choosedCore = Console.ReadLine();
+                    Console.WriteLine("Choose legs from existing: ");
+                    foreach (string legs in viewModel.ExistingLegs)
+                    {
+                        Console.WriteLine(legs);
+                    }
+                    string choosedLegs = Console.ReadLine();
+
+                    var robot = viewModel.CreateRobot(robotName, choosedArms, choosedBody, choosedCore, choosedLegs);
+                    Console.WriteLine($"{robotName} created. Return to menu.");
+                    Thread.Sleep(2000);
+                    break;
+            }
+            Console.Clear();
+        }
         Console.WriteLine(viewModel.FormattedReport);
+    }
+
+    private static string DisplayMenuAndChoiceOfOption(ViewModel viewModel)
+    {
+        Console.WriteLine("Choose number of option from menu: ");
+        foreach (string option in viewModel.OptionsMenu)
+        {
+            Console.WriteLine(option);
+        }
+        return Console.ReadLine();
     }
 }
--- RobotViewModels/ViewModel.cs
+++ RobotViewModels/ViewModel.cs
@@ -1,7 +1,103 @@
-﻿namespace RobotViewModels
+﻿using RobotApp.RobotData.RobotEquipment.ArmsTypes;
+using RobotApp.RobotData.RobotEquipment.BodyTypes;
+using RobotApp.RobotData.RobotEquipment.CoreTypes;
+using RobotApp.RobotData.RobotEquipment.LegsTypes;
+using RobotApp.RobotData;
+using RobotApp.Services.Reports;
+using RobotApp.Services;
+using RobotViewModels.Formatters;
+using RobotViewModels.Interfaces;
+using RobotApp.RobotData.RobotParts;
+using System;
+
+namespace RobotViewModels
 {
-    public class ViewModel 
+    public class ViewModel
     {
         public string FormattedReport { get; set; }
+
+        public List<string> OptionsMenu { get; set; } = new List<string>()
+        {
+            "1. Create two robots", "2. Create report", "3. Exit"
+        };
+
+        public List<string> ExistingArms { get; set; } = new List<string>()
+        {
+            "RocketArms", "SpearArms"
+        };
+
+        public List<string> ExistingBodies { get; set; } = new List<string>()
+        {
+            "ShieldedBody", "TankyBody"
+        };
+
+        public List<string> ExistingCores { get; set; } = new List<string>()
+        {
+            "EnergeticCore", "LivingCore"
+        };
+
+        public List<string> ExistingLegs { get; set; } = new List<string>()
+        {
+            "SpeedLegs", "ArmouredLegs"
+        };
+
+        public ViewModel()
+        {
+        }
+
+        public string CreateAndFormatComparisonReport(Robot robot1, Robot robot2)
+        {
+            CompareRobotCharacteristicsService compareRobotCharacteristicsService = new();
+            IRobotsComparisonFormatter comparisonFormatter = new ReportFormatter();
+
+            RobotComparisonReport report = compareRobotCharacteristicsService.CreateRobotComparisonReport(robot1, robot2);
+
+            return comparisonFormatter.Format(report);
+        }
+
+        public object CreateRobot(string robotName, string choosedArms, string choosedBody,
+            string choosedCore, string choosedLegs)
+        {
+            Robot robot = new(robotName);
+
+            switch (choosedArms)
+            {
+                case "RocketArms":
+                    robot.AddArms(new RocketArms());
+                    break;
+                case "SpearArms":
+                    robot.AddArms(new SpearArms());
+                    break;
+            }
+            switch (choosedBody)
+            {
+                case "ShieldedBody":
+                    robot.AddBody(new ShieldedBody());
+                    break;
+                case "TankyBody":
+                    robot.AddBody(new TankyBody());
+                    break;
+            }
+            switch (choosedCore)
+            {
+                case "EnergeticCore":
+                    robot.AddCore(new EnergeticCore());
+                    break;
+                case "LivingCore":
+                    robot.AddCore(new LivingCore());
+                    break;
+            }
+            switch (choosedLegs)
+            {
+                case "SpeedLegs":
+                    robot.AddLegs(new SpeedLegs());
+                    break;
+                case "ArmouredLegs":
+                    robot.AddLegs(new ArmouredLegs());
+                    break;
+            }
+
+            return robot;
+        }
     }
 }
