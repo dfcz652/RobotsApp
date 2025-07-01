4c8b482a1ee18ee7988862755f1f52438ec62517
Andrey <sandriys2003@gmail.com>
2025-07-01 19:01:20 +0300

    Implemented IRobotsGateway, refactored Program and ViewModel. (#24)

---
+++ RobotApp/Services/IRobotsGateway.cs
@@ -0,0 +1,14 @@
+﻿using RobotApp.RobotData;
+
+namespace RobotApp.Services
+{
+    public interface IRobotsGateway
+    {
+        int Count {  get; }
+        void Add(Robot robot);
+        void Clear();
+        Robot GetByName(string name);
+
+        List<Robot> GetAll();
+    }
+}
---
+++ RobotApp/Services/RobotsGatewayInMemory.cs
@@ -0,0 +1,31 @@
+﻿using RobotApp.RobotData;
+
+namespace RobotApp.Services
+{
+    public class RobotsGatewayInMemory : IRobotsGateway
+    {
+        private List<Robot> _robots = new();
+
+        public int Count => _robots.Count();
+
+        public void Add(Robot robot)
+        {
+            _robots.Add(robot);
+        }
+
+        public void Clear()
+        {
+            _robots.Clear();
+        }
+
+        public Robot GetByName(string name)
+        {
+            return _robots.FirstOrDefault(r => r.Name == name);
+        }
+
+        public List<Robot> GetAll()
+        {
+            return _robots;
+        }
+    }
+}
